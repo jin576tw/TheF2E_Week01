7407b3fe7441b20b9d46ac16887d38d11e5f5e01
jin576tw <jin576tw@gmail.com>
2021-11-14 01:11:18 +0800

    資料串接中

--- static/js/common.js
+++ static/js/common.js
@@ -1,43 +1,35 @@
+
+// 使用api
 function getAuthorizationHeader() {
-    //  填入自己 ID、KEY 開始
-        let AppID = '2d9b9a569dab44dfbf33035ed2846ac6';
-        let AppKey = '0Ly8nNY_Mw4fL3dcFmKXx4y1mZo';
-    //  填入自己 ID、KEY 結束
-        let GMTString = new Date().toGMTString();
-        let ShaObj = new jsSHA('SHA-1', 'TEXT');
-        ShaObj.setHMACKey(AppKey, 'TEXT');
-        ShaObj.update('x-date: ' + GMTString);
-        let HMAC = ShaObj.getHMAC('B64');
-        let Authorization = 'hmac username=\"' + AppID + '\", algorithm=\"hmac-sha1\", headers=\"x-date\", signature=\"' + HMAC + '\"';
-        return { 'Authorization': Authorization, 'X-Date': GMTString }; 
+  //  填入自己 ID、KEY 開始
+  let AppID = "2d9b9a569dab44dfbf33035ed2846ac6";
+  let AppKey = "0Ly8nNY_Mw4fL3dcFmKXx4y1mZo";
+  //  填入自己 ID、KEY 結束
+  let GMTString = new Date().toGMTString();
+  let ShaObj = new jsSHA("SHA-1", "TEXT");
+  ShaObj.setHMACKey(AppKey, "TEXT");
+  ShaObj.update("x-date: " + GMTString);
+  let HMAC = ShaObj.getHMAC("B64");
+  let Authorization =
+    'hmac username="' +
+    AppID +
+    '", algorithm="hmac-sha1", headers="x-date", signature="' +
+    HMAC +
+    '"';
+  return { Authorization: Authorization, "X-Date": GMTString };
 }
 
+getAuthorizationHeader()
 
 
-axios.get(
-    'https://ptx.transportdata.tw/MOTC/v2/Rail/TRA/Station?$top=10&$format=JSON',
-    {
-       headers: getAuthorizationHeader()
-    }
- )
- .then(function (response) {
-   document.querySelector('body').textContent=JSON.stringify(response.data);
- })
- .catch(function (error) {
-   console.log(error);
- });
 let nowPage = ''
-
-$('.search_btn').click(function(){
-
+let nowTIME = new Date('2016-05-10').getDate()
 
 
 
 
-});
+console.log(nowTIME);
 
-$('.Hotel_Btn').click(function(){
 
-   
 
-})
+
